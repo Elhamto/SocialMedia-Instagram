2cf34812d0f327b65a4043fc92e928fe38707310
Elhamto <elham.toloeii@gmail.com>
2022-11-14 15:29:23 +0330

    */dist

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,9 +1,52 @@
-import { IsEmail, IsNotEmpty } from 'class-validator';
+import {
+  IsArray,
+  IsBoolean,
+  IsEmail,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  Validate,
+} from 'class-validator';
+import { User, UserDocument } from '../schema/user.schema';
 
 export class CreateUserDto {
-  @IsEmail()
-  email: string;
+  //   @Validate(IsUnique, { message: 'Isnot unique name' })
+  @IsNotEmpty()
+  @IsString()
+  username: string;
 
   @IsNotEmpty()
+  @IsString()
   password: string;
+
+  @IsEmail()
+  @IsOptional()
+  email: string;
+
+  @IsString()
+  @IsOptional()
+  phoneNumber: string;
+
+  @IsArray()
+  @IsOptional()
+  followers: UserDocument;
+
+  @IsArray()
+  @IsOptional()
+  followings: UserDocument;
+
+  @IsArray()
+  @IsOptional()
+  closeUsers: UserDocument;
+
+  @IsArray()
+  @IsOptional()
+  hideUsers: UserDocument;
+
+  @IsArray()
+  @IsOptional()
+  blockUsers: UserDocument;
+
+  @IsBoolean()
+  visiblity: boolean;
 }

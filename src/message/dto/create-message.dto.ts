b6119e5258d6d9e2f2800d349a2189aeffb818c8
Elhamto <elham.toloeii@gmail.com>
2022-11-14 17:54:53 +0330

    create StorySchema

--- src/message/dto/create-message.dto.ts
+++ src/message/dto/create-message.dto.ts
@@ -1,1 +1,27 @@
-export class CreateMessageDto {}
+import {
+  IsBoolean,
+  IsDate,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+} from 'class-validator';
+
+export class CreateMessageDto {
+  @IsNotEmpty()
+  from: any;
+
+  @IsNotEmpty()
+  to: any;
+
+  @IsString()
+  @IsNotEmpty()
+  content: string;
+
+  @IsDate()
+  @IsNotEmpty()
+  date: Date;
+
+  @IsBoolean()
+  @IsOptional()
+  liked: boolean;
+}
--- src/story/schema/story.schema.ts
+++ src/story/schema/story.schema.ts
@@ -1,8 +1,25 @@
-import mongoose from 'mongoose';
-const { Schema } = mongoose;
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import * as mongoose from 'mongoose';
+import { User } from 'src/user/schema/user.schema';
 
-export const StorySchema = new Schema({
-  owner: { type: Schema.Types.ObjectId, ref: 'User' },
-  content: String, //url
-  expiration: Date,
-});
+export type StoryDocument = mongoose.HydratedDocument<Story>;
+
+@Schema()
+export class Story {
+  @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'User' })
+  owner: User;
+
+  @Prop()
+  content: string; //url
+
+  @Prop()
+  expiration: Date; //lazeme? nemishe hamaro 24h goft?
+
+  @Prop({ type: [{ type: mongoose.Schema.Types.ObjectId }] })
+  likes: User[];
+
+  // @Prop()
+  // reply: string;
+}
+
+export const StorySchema = SchemaFactory.createForClass(Story);

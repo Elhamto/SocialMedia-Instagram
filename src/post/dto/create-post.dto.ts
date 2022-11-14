eac6371e57b194168e58eb78404d82862586a89f
Elhamto <elham.toloeii@gmail.com>
2022-11-14 15:47:53 +0330

    CreatePostDto

--- src/post/dto/create-post.dto.ts
+++ src/post/dto/create-post.dto.ts
@@ -1,1 +1,34 @@
-export class CreatePostDto {}
+import {
+  IsArray,
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+  IsString,
+  IsUrl,
+} from 'class-validator';
+import { UserDocument } from 'src/user/schema/user.schema';
+
+export class CreateUserDto {
+  //   @Validate(IsUnique, { message: 'Isnot unique name' })
+  @IsNotEmpty()
+  owner: UserDocument;
+
+  @IsUrl() // @IsString()
+  @IsNotEmpty()
+  content: string;
+
+  @IsString()
+  @IsOptional()
+  caption: string;
+
+  @IsOptional()
+  comments: Array<any>;
+
+  @IsArray()
+  @IsOptional()
+  likes: UserDocument;
+
+  @IsNumber()
+  @IsOptional()
+  viewCount: number;
+}

75f0119418e4929a4e7d9c530f6dca178cbe86b9
Elhamto <elham.toloeii@gmail.com>
2022-11-14 17:54:34 +0330

    CreateStoryDto

--- src/story/dto/create-story.dto.ts
+++ src/story/dto/create-story.dto.ts
@@ -1,1 +1,27 @@
-export class CreateStoryDto {}
+import {
+  IsArray,
+  IsDate,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+} from 'class-validator';
+
+export class CreateStoryDto {
+  //   @Validate(IsUnique, { message: 'Isnot unique name' })
+  @IsNotEmpty()
+  owner: any;
+
+  @IsString()
+  @IsNotEmpty()
+  content: string; //url
+
+  @IsDate()
+  expiration: Date; //lazeme? nemishe hamaro 24h goft?
+
+  @IsArray()
+  @IsOptional()
+  likes: any;
+
+  // @IsString()
+  // reply: string;
+}

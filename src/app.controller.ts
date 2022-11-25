cc1440a9a7382fac5255b8760e9c20b3c80647b8
Elhamto <elham.toloeii@gmail.com>
2022-11-25 13:50:09 +0330

    add register

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,15 +1,31 @@
-import { Controller, Request, Get, Post, UseGuards } from '@nestjs/common';
+import {
+  Controller,
+  Request,
+  Get,
+  Post,
+  UseGuards,
+  Body,
+} from '@nestjs/common';
 import { AppService } from './app.service';
 import { AuthService } from './auth/auth.service';
 import { LocalAuthGuard } from './auth/local-auth.guard';
 import { JwtAuthGuard } from './auth/jwt-auth.guard';
+import { UserService } from './user/user.service';
+import { CreateUserDto } from './user/dto/user.dto';
 
 @Controller()
 export class AppController {
   constructor(
     private readonly appService: AppService,
     private authService: AuthService,
+    private userService: UserService,
   ) {}
+
+  @Post('register')
+  async register(@Body() createUserDto: CreateUserDto) {
+    const reg = await this.userService.create(createUserDto);
+    return await this.authService.login(reg);
+  }
 
   @UseGuards(LocalAuthGuard)
   @Post('login')
@@ -21,6 +37,5 @@ export class AppController {
   @Get('secret')
   getProfile(@Request() req) {
     return req.user;
-    // return `hello ${JSON.stringify(req.user)}`;
   }
 }
